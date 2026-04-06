93dc8e3b775d287d716095f15a86eb7da5804d29
Tim Halmshaw <timhalmshaw@gmail.com>
2026-04-06 16:41:25 +0100

    RED: Setup mocking framework.  Add first test to calculate correct price of £25 for adult.

---
+++ src/test/java/uk/gov/dwp/uc/pairtest/TicketServiceImplTest.java
@@ -0,0 +1,29 @@
+package uk.gov.dwp.uc.pairtest;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import thirdparty.paymentgateway.TicketPaymentService;
+import thirdparty.paymentgateway.TicketPaymentServiceImpl;
+import thirdparty.seatbooking.SeatReservationService;
+import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
+
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class TicketServiceImplTest {
+
+    @Mock private TicketPaymentService paymentService;
+    @Mock private SeatReservationService seatReservationService;
+    @InjectMocks private TicketServiceImpl ticketService;
+
+    @Test
+    void shouldCalculateCorrectPriceForSingleAdultTicket() {
+        ticketService.purchaseTickets(1L,
+            new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 1));
+
+        verify(paymentService).makePayment(1L, 25);
+    }
+}
