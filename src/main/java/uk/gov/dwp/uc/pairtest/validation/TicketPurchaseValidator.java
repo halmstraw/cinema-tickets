efa06eac52c58ede2d577c3f680d186203dfc6b1
Tim Halmshaw <timhalmshaw@gmail.com>
2026-04-06 14:39:33 +0100

    RED: First failing test added for when account id is zero

---
+++ src/main/java/uk/gov/dwp/uc/pairtest/validation/TicketPurchaseValidator.java
@@ -0,0 +1,11 @@
+package uk.gov.dwp.uc.pairtest.validation;
+
+import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
+import uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
+
+public class TicketPurchaseValidator {
+
+    public void validate(Long accountId, TicketTypeRequest... ticketTypeRequests) throws InvalidPurchaseException {
+    }
+
+}
---
+++ src/test/java/uk/gov/dwp/uc/pairtest/validation/TicketPurchaseValidatorTest.java
@@ -0,0 +1,16 @@
+package uk.gov.dwp.uc.pairtest.validation;
+
+import org.junit.jupiter.api.Test;
+import uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class TicketPurchaseValidatorTest {
+
+    private final TicketPurchaseValidator validator = new TicketPurchaseValidator();
+
+    @Test
+    void shouldRejectPurchaseWhenAccountIDIsZero() {
+        assertThrows(InvalidPurchaseException.class, () -> validator.validate(0L));
+    }
+}
